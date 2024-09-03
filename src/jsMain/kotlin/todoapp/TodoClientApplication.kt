9389ffbfc9a68ae34aa5a8cbacef3d8fa8e5f478
gabeen <gabeen.kim@outlook.com>
2024-09-03 10:23:31 +0900

    kotlin - react

--- src/jsMain/kotlin/todoapp/TodoClientApplication.kt
+++ src/jsMain/kotlin/todoapp/TodoClientApplication.kt
@@ -4,6 +4,9 @@ import kotlinx.browser.document
 import kotlinx.html.dom.create
 import kotlinx.html.h1
 import kotlinx.html.js.div
+import react.create
+import react.dom.client.createRoot
+import todoapp.ui.welcome.WelcomePage
 
 /**
  * 클라이언트 애플리케이션 진입점(entry point)
@@ -13,11 +16,19 @@ import kotlinx.html.js.div
 fun main() {
     val container = document.getElementById("root") ?: error("Couldn't find root container!")
 
-    container.appendChild (
-        document.create.div {
-            h1 {
-                +"Hello, Kotlin/js"
-            }
+//  React
+    createRoot(container = container).render(
+        WelcomePage.create {
+            name = "Gabeen Kim"
         }
     )
+
+//    js
+//    container.appendChild (
+//        document.create.div {
+//            h1 {
+//                +"Hello, Kotlin/js"
+//            }
+//        }
+//    )
 }
--- src/jsMain/kotlin/todoapp/ui/welcome/WelcomePage.kt
+++ src/jsMain/kotlin/todoapp/ui/welcome/WelcomePage.kt
@@ -2,12 +2,23 @@ package todoapp.ui.welcome
 
 import csstype.*
 import emotion.react.css
+import kotlinx.html.InputType
 import react.FC
 import react.Props
+import react.dom.html.ReactHTML
 import react.dom.html.ReactHTML.div
+import react.dom.html.ReactHTML.input
 import react.dom.html.ReactHTML.section
+import react.useState
 
-val WelcomePage = FC<Props> {
+// 외부 인터페이스
+external interface WelcomePageProps : Props {
+    var name: String?
+}
+
+val WelcomePage = FC<WelcomePageProps> {
+    val (name, setName) = useState(it.name?: "SpringRunner")
+
     section {
         css {
             position = Position.absolute
@@ -20,7 +31,16 @@ val WelcomePage = FC<Props> {
                 fontSize = 20.px
                 fontWeight = FontWeight.bold
             }
-            +"Hello, SpringRunner"
+            +"Hello, $name"
+        }
+
+        ReactHTML.input {
+            type = react.dom.html.InputType.text
+            value = name
+            onChange = { event ->
+                setName(event.currentTarget.value)
+            }
+
         }
     }
 }
