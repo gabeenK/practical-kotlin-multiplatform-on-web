db834dcb944f8a4690170bdc27d5c39d5ab0e41b
gabeen <gabeen.kim@outlook.com>
2024-09-03 10:55:50 +0900

    kotlin - react

--- src/jsMain/kotlin/todoapp/TodoClientApplication.kt
+++ src/jsMain/kotlin/todoapp/TodoClientApplication.kt
@@ -1,12 +1,9 @@
 package todoapp
 
 import kotlinx.browser.document
-import kotlinx.html.dom.create
-import kotlinx.html.h1
-import kotlinx.html.js.div
 import react.create
 import react.dom.client.createRoot
-import todoapp.ui.welcome.WelcomePage
+import todoapp.ui.App
 
 /**
  * 클라이언트 애플리케이션 진입점(entry point)
@@ -16,19 +13,7 @@ import todoapp.ui.welcome.WelcomePage
 fun main() {
     val container = document.getElementById("root") ?: error("Couldn't find root container!")
 
-//  React
     createRoot(container = container).render(
-        WelcomePage.create {
-            name = "Gabeen Kim"
-        }
+        App.create()
     )
-
-//    js
-//    container.appendChild (
-//        document.create.div {
-//            h1 {
-//                +"Hello, Kotlin/js"
-//            }
-//        }
-//    )
 }
